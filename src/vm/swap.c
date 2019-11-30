7bfc30e71832a8baa747d939d12ec008c9fd59be
头 <qiaowh@shanghaitech.edu.cn>
2019-11-30 15:36:22 +0800

    add comment to swap

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -6,9 +6,11 @@ swap_init(){
     swap_block = block_get_role(BLOCK_SWAP);
 	if (swap_block == NULL)
 		return;
+	/* Creat the bitmap to manage the swap slot. */
 	swap_map = bitmap_create(block_size(swap_block) / SEC_NUM_PER_PAGE);
 	if (swap_map == NULL)
 		return;
+	/* Set the bit to 1, indicate free to use. */
 	bitmap_set_all(swap_map, true);
 }
 
@@ -19,10 +21,13 @@ swap_init(){
    Return the */
 unsigned int
 swap_write_out(void* frame_addr){
+	/* Get the index for swap. */
 	size_t first_free = bitmap_scan_and_flip(swap_map, 0, 1, 1);
+	/* Write to the block device. */
 	for (unsigned int i = 0; i < SEC_NUM_PER_PAGE; i++)
 		block_write(swap_block, first_free * SEC_NUM_PER_PAGE + i,
 			frame_addr + i * BLOCK_SECTOR_SIZE);
+	/* Return the index of write. */
 	return first_free;
 }
 
@@ -32,9 +37,11 @@ swap_write_out(void* frame_addr){
    it succeed to do so. */
 void
 swap_read_in(unsigned int idx, void* frame_addr){
+	/* Read the data from block device. */
 	for (unsigned int i = 0; i < SEC_NUM_PER_PAGE; i++)
 		block_read(swap_block, idx*SEC_NUM_PER_PAGE+i,
 						frame_addr + i*BLOCK_SECTOR_SIZE);
+	/* Set the index back to free. */
 	bitmap_set(swap_map, idx, true);
-	return true;
+	return;
 }
